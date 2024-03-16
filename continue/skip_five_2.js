c1cad6c80e6ea17b852e47d6fff3612e24c834e0
Razib2019 <hafizurrahmanrazib@gmail.com>
2024-03-16 23:13:47 +0600

    continue problems solving done

--- continue/skip_five_2.js
+++ continue/skip_five_2.js
@@ -1,3 +1,16 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
+let n = 55;
+while (n <= 85) {
+  if (n % 2 !== 0 && n % 5 !== 0) {
+    console.log(
+      "Odd number from 55 to 85 and skip the numbers divisible by 5:",
+      n
+    );
+    n++;
+  } else {
+    n++;
+    continue;
+  }
+}
--- continue/skip_odd_1.js
+++ continue/skip_odd_1.js
@@ -1,3 +1,14 @@
 /* 
 Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
 */
+
+let n = 1;
+while (n <= 40) {
+  if (n % 2 === 0) {
+    console.log("Even numbers from 1 to 40:", n);
+    n++;
+  } else {
+    n++;
+    continue;
+  }
+}
