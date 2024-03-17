7f66b11a708def88f99caddc95216b068e82ad81
Razib2019 <hafizurrahmanrazib@gmail.com>
2024-03-17 07:05:56 +0600

    break problem solving almost done

--- break/square_break_3.js
+++ break/square_break_3.js
@@ -1,3 +1,19 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+// let n = 1;
+// while (n <= 10) {
+//   if(n)
+//   console.log(n);
+//   n++;
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+//   let square = i * i;
+//   if (square !== 1 || square !== square / i) {
+//     break;
+//   }
+// }
--- break/sum_upto_100_2.js
+++ break/sum_upto_100_2.js
@@ -1,3 +1,13 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let sum = 0;
+let n = 1;
+while (sum <= 100) {
+  sum += n;
+  console.log(sum);
+  n++;
+  if (sum >= 100) {
+    break;
+  }
+}
